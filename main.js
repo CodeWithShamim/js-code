11b85ca52f120b4c07fc5afe5159b315eb19b2e0
codewithshamim <shamimislamonline@gmail.com>
2022-01-26 00:09:22 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Variable declaraton
+var onionPrice = 110;
+var eggPrice = 50;
+var totalPriceAdd = onionPrice + eggPrice;
+var totalPriceSub = onionPrice - eggPrice;
+var totalPriceMul = onionPrice * eggPrice;
+var totalPriceDiv = onionPrice / eggPrice;
+var totalPriceEx = onionPrice ** eggPrice;
+
+console.log(totalPriceAdd, totalPriceSub, totalPriceMul, totalPriceDiv, totalPriceEx)
+// console.log(totalPriceSub)
+// console.log(totalPriceMul)
+// console.log(totalPriceDiv)
+// console.log(totalPriceEx)
