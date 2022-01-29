f90363b1f303a7b9c4ed1d577477bea0352525d5
codewithshamim <shamimislamonline@gmail.com>
2022-01-29 15:53:37 +0600

    fn added

---
+++ Module 19 ( Function & object )/Switch.js
@@ -0,0 +1,27 @@
+// switch case break default
+
+function numbers(num){
+    switch(num){
+        case "a":
+            console.log("Vowel");
+            break;
+        case "e":
+            console.log("Vowel");
+            break;
+        case "i":
+            console.log("Vowel");
+            break;
+        case "o":
+            console.log("Vowel");
+            break;
+        case "u":
+            console.log("Vowel");
+            break;
+        default:
+            console.log("Consonant");
+    }
+}
+
+var pass1 = "A";
+pass1 = pass1.toLowerCase();
+numbers(pass1);
