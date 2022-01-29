f35ffd79945d8f322a114af40a20e7e08a369388
codewithshamim <shamimislamonline@gmail.com>
2022-01-29 20:28:49 +0600

    added

---
+++ Module 20 ( Apply js concept)/let-const.js
@@ -0,0 +1,12 @@
+// Value of variable could change (let)
+let mobileBalance = 10;
+mobileBalance = 20;
+mobileBalance = 30;
+console.log(mobileBalance);
+
+// Value of the variable will not change (const)
+const myName = 'shamim';
+// can't change value
+// myName = 'rafiq';
+console.log(myName);
+
