c0d5651d0e68477d19a834f19da246dbea116e9a
codewithshamim <shamimislamonline@gmail.com>
2022-01-31 21:27:47 +0600

    math added

---
+++ Module 21 (JS coding problem)/math.js
@@ -0,0 +1,30 @@
+// ===========abs, ceil, floor, round, pow, sqrt, min, max, random============
+// const myNumber = -5;
+// const result = Math.abs(myNumber);
+// const myNumber = 21.04;
+// const result = Math.ceil(myNumber);
+// const myNumber = 21.04;
+// const result = Math.floor(myNumber);
+// let myNumber = 21.04;
+// myNumber = 21.50;
+// const result = Math.round(myNumber);
+
+/*
+for (i=1; i<=30; i++){
+    let myNumber = 21.04;
+    let result = (Math.random()*5)+1;
+    result = Math.floor(result);
+    console.log(result);
+};
+
+*/
+
+
+// const result = Math.pow(2,3);
+// console.log(result);
+
+// const result = Math.min(2, 3, 54, 1);
+// console.log(result);
+
+const result = Math.max(2, 3, 54, 1);
+console.log(result);
