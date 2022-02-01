441c02689e7379e6bf7a2c8d5916d8a12721bd6f
codewithshamim <shamimislamonline@gmail.com>
2022-02-01 20:10:41 +0600

    Find largest number in array

--- Module 21 (JS coding problem)/largest-element.js
+++ Module 21 (JS coding problem)/largest-element.js
@@ -1,5 +1,5 @@
 function isSumation(items){
-    let largest = 0;
+    let largest = items[0];
     for (i=0; i<items.length; i++){
         let itemSum = items[i];
         if ( itemSum>largest){
@@ -9,5 +9,6 @@ function isSumation(items){
     return largest;
 }
 let numbers = [10, 20, 30, 40, 50, 600, 70, 80, 90, 100];
+numbers = [-12, -6, -16];
 let result = isSumation(numbers);
 console.log("Largest number is: "+ result);
