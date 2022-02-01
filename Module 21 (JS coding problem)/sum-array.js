a5ca96575abde57481da3aa83cfd18d9f7b160d1
codewithshamim <shamimislamonline@gmail.com>
2022-02-01 18:36:24 +0600

    sumation from array number

---
+++ Module 21 (JS coding problem)/sum-array.js
@@ -0,0 +1,8 @@
+let items = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+let sum = 0;
+for (i=0; i<items.length; i++){
+    let itemSum = items[i];
+    sum = sum +itemSum;
+}
+
+console.log("Total sumation is: "+ sum)
