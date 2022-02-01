de8143d04bc3c372707468b54e17f3a54178cac1
codewithshamim <shamimislamonline@gmail.com>
2022-02-01 18:49:07 +0600

    Sumation from array number using FN

--- Module 21 (JS coding problem)/sum-array.js
+++ Module 21 (JS coding problem)/sum-array.js
@@ -1,8 +1,12 @@
-let items = [10, 20, 30, 40, 50, 60, 70, 80, 90];
-let sum = 0;
-for (i=0; i<items.length; i++){
-    let itemSum = items[i];
-    sum = sum +itemSum;
+function isSumation(items){
+    let sum = 0;
+    for (i=0; i<items.length; i++){
+        let itemSum = items[i];
+        sum = sum +itemSum;
+        
+    }
+    return sum;
 }
-
-console.log("Total sumation is: "+ sum)
+let numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+let result = isSumation(numbers);
+console.log("Total sumation is: "+ result);
