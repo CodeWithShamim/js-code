9319b1d4dfc06cd98b052a7d739b8ca64034ea07
codewithshamim <shamimislamonline@gmail.com>
2022-02-03 17:56:00 +0600

    problem solving

---
+++ Conceptual session(problem solving)/problem.js
@@ -0,0 +1,35 @@
+/* ৪. একটা ফাংশন লিখবে। এই ফাংশনের নাম হবে bestFriend তারপর সেই ফাংশনে ইনপুট প্যারামিটার হিসেবে একটা array নিবে। সেই array এর মধ্যে তোমার সব ফ্রেন্ডের নাম থাকবে। এখন তোমার কাজ হচ্ছে যে ফ্রেন্ড এর নাম সবচেয়ে বড় সেই ফ্রেন্ড এর নাম রিটার্ন করে দেয়া। এই ক্ষেত্রে তুমি নামটা অর্থাৎ ফ্রেন্ডের নাম (স্ট্রিং) রিটার্ন করতে হবে। */
+
+const namesArray = ['shamim', 'salman', 'shahinn islam', 'hasmot', 'jubaidul', 'shamim islam'];
+const result = bestFriend(namesArray);
+console.log(result);
+function bestFriend(names){
+    let largeName = "";
+    for (let name of names){
+        if (name.length>largeName.length){
+            largeName = name;
+        }
+    }
+    return largeName;
+
+};
+
+
+/*
+৫. এইটা একটু ট্রিকি হতে পারে। একটা array এর মধ্যে অনেকগুলা সংখ্যা থাকবে। তোমার কাজ হচ্ছে সংখ্যা গুলা একটার পর একটা করে চেক করা। এবং সংখ্যা গুলা যদি পজিটিভ সংখ্যা হয়। অর্থাৎ শূন্য বা শূন্যের চাইতে বড় হয় তাহলে সেগুলাকে কোন একটা array এর মধ্যে রাখবে। আর যদি নেগেটিভ সংখ্যা হয়। তাহলে লুপটা স্টপ করে দিবে। এবং লুপ বন্ধ করার আগ পর্যন্ত যতগুলা পজিটিভ সংখ্যা পাওয়া গেছে। সেগুলা রিটার্ন করে দিবে। */
+
+const numbers = [10, 5, 3, 4, 50, -1, 6, 1, 3, 2, 0, -1, -3, -2];
+isPositive(numbers);
+function isPositive(numbers){
+    let positiveNumber = [];
+    for (let number of numbers){  
+        if (number>0){
+            positiveNumber.push(number);
+        }
+        else{
+            break;
+        }
+    }
+    console.log(positiveNumber)
+}
+
